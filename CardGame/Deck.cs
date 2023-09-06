872ffcd6cb2744a0a72eb8f87b5a4b76afdcd2ad
User <User@LAB3-3>
2023-09-06 11:25:50 +0300

    save

--- CardGame/Deck.cs
+++ CardGame/Deck.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,31 @@ namespace CardGame
     public class Deck
     {
         private Card[] cards;
+        private int top;
 
-        public Deck() { }
+        public Deck() 
+        { 
+        cards = new Card[52];
+            int val = 0;
+            for (int i=0; i<52; i++)
+            {
+                cards[i] = new Card(((val % 13) + 1), ((val / 13) + 1));
+                    val++;
+            }
+            top = 0;
+        }
         public Card Peek() { return null; }
         public void Shuffle() { }
-        public Card Deal() { return null; }
+        public Card Deal() 
+        {
+            Card newcard = cards[top];
+            top++;
+            return newcard;
+        }
 
-        public void Reset() { }
+        public void Reset() 
+        {
+            top= 0;
+        }
     }
 }
